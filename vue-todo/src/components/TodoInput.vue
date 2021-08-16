42946457e23c184cde6f68660cb9cf086d278f38
florence1920 <jetaime12121@gmail.com>
2021-08-17 00:57:45 +0900

    todo list, footer

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -16,9 +16,12 @@ export default {
     },
     methods :{
         addTodo: function(){
-            //저장하는 로직
-            localStorage.setItem(this.newTodoItem,this.newTodoItem);
-            this.clearInput();
+            if(this.newTodoItem !== ''){
+                //저장하는 로직
+                let obj = {completed: false, item: this.newTodoItem};
+                localStorage.setItem(this.newTodoItem,JSON.stringify(obj));
+                this.clearInput();
+            }
         },
         clearInput:function(){
             this.newTodoItem='';
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
         <ul>
-            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
-                {{ todoItem }}
+            <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+                <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
+                <span v-bind:class="{textCompleted: todoItem.completed}"> {{ todoItem.item }} </span>
                 <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
                     <i class="far fa-trash-alt"></i>
                 </span>
@@ -23,13 +24,19 @@ export default {
             console.log(todoItem, index);
             localStorage.removeItem(todoItem);
             this.todoItems.splice(index, 1);
+        },
+        toggleComplete : function(todoItem){
+            todoItem.completed = !todoItem.completed;
+            //local storage 갱신 
+            localStorage.removeItem(todoItem.item);
+            localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
         }
     },
     created: function(){
         if(localStorage.length > 0){
             for(var i = 0; i < localStorage.length; i++){
                 if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-                    this.todoItems.push(localStorage.key(i));
+                    this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
                 }
             }
         }
@@ -43,6 +50,6 @@ li {display: flex; min-height: 50px; height: 50px; line-height: 50px; margin:0.5
 .removeBtn {margin-left: auto; color:#de4343;}
 .checkBtn {line-height:45px; color:#62acde; margin-right:5px;}
 .checkBtnCompleted {color:#b3adad;}
-.textCompleted {text-decoration: none; color:#b3adad;}
+.textCompleted {text-decoration: line-through; color:#b3adad;}
 
 </style>
